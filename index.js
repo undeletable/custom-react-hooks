5823c298391a92729c7d9d27f31d551cb74ce830
Max Shepel <max@undeletable.name>
2022-10-25 16:59:39 +0300

    Fix main export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import useWindowHeight from './src/hooks/useWindowHeight';
 import useWindowWidth from './src/hooks/useWindowWidth';
 
-export default {useWindowHeight, useWindowWidth};
+export {useWindowHeight, useWindowWidth};
